30d62ad8e9e4c7e94ae3a00bdf9dd751a7315335
Kian Lund <lundk90@gmail.com>
2023-04-11 14:55:42 +0200

    Test db connection.

--- src/main/java/com/example/fullstackproject/controller/HomeController.java
+++ src/main/java/com/example/fullstackproject/controller/HomeController.java
@@ -1,13 +1,27 @@
 package com.example.fullstackproject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class HomeController {
+
+    @Autowired
+    JdbcTemplate jdbctempplate;
+
     @GetMapping("/")
     public String index(){
         return "/index";
     }
+
+    @GetMapping("test")
+    public String test(){
+        String sql = "INSERT INTO accounts(username, password) VALUES ('test', 'test2')";
+        jdbctempplate.update(sql);
+        return "/index";
+    }
+
 }
