74554e600c05f79adce698de763e6daa81bdcd03
Poijus <92998600+Poijus@users.noreply.github.com>
2023-04-13 13:50:43 +0200

    fetchAllWishLists metode

--- src/main/java/com/example/fullstackproject/controller/HomeController.java
+++ src/main/java/com/example/fullstackproject/controller/HomeController.java
@@ -1,27 +1,13 @@
-package com.example.fullstackproject.controller;
+package com.example.fullstackproject.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class HomeController {
-
-    @Autowired
-    JdbcTemplate jdbctempplate;
 
     @GetMapping("/")
     public String index(){
-        return "/index";
+        return "Home/index.html";
     }
-
-    @GetMapping("test")
-    public String test(){
-        String sql = "INSERT INTO accounts(username, password) VALUES ('test', 'test2')";
-        jdbctempplate.update(sql);
-        return "/index";
-    }
-
 }
